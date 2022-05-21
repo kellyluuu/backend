c578d2c0108288120071ef48233032137d360f68
KELLY LUU <kellyluuu@gmail.com>
2022-05-21 13:21:16 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+// import dependencies 
+const express =  require("express")
+const cors = require("cors")
+
+//import JSON files
+const projects = require("./projects.json")
+const about = require("./about.json")
+
+// create our app object
+const app = express()
+
+//setup middleware
+app.use(cors())
+
+//home route for testing our app
+app.get('/', (req,res)=>{
+    res.send("Hello World")
+})
+
+// route for retrieving projects 
+app.get("/projects", (req,res)=>{
+    //send project via JSON
+    res.json(projects);
+})
+
+// route for retrieving projects
+app.get("/projects",(req,res)=>{
+    //send projects via JSON
+    res.json(projects);
+})
+
+//route for retrieving about info 
+app.get("/about",(req,res)=>{
+    //send projects vis JSON
+    res.json(about)
+})
+
+//declare a variable for our port number 
+const PORT = process.env.PORT || 4000
+
+//turn on the server listener
+app.listen(PORT, ()=> console.log(`listening on port ${PORT}`))
+
+
